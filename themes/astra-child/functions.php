1961ecf8739e0912620c194469b6ea10b043b1b6
Marine Moser <moser.marine@gmail.com>
2023-11-20 17:13:25 +0100

    css and hook

--- themes/astra-child/functions.php
+++ themes/astra-child/functions.php
@@ -27,24 +27,23 @@ add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
 
 // hook
 function ajouter_lien_admin_menu_astra( $items, $args ) {
-	
-    // Vérifie si l'utilisateur est connecté
-    if ( is_user_logged_in() && $args->menu == 7 ) {
-        // Balises <li> pour "Nous rencontrer"
-        // $rencontrer_li = '<li class="menu-item"><a href"Nous rencontrer"</li>';
-        
+
+    // Vérifie si l'utilisateur est connecté 
+
+    if ( is_user_logged_in() && $args->theme_location == "primary" || $args->theme_location == "off_canvas") {
+
         // Balises <li> pour "Admin"
-		$items .= '<li class="menu-item"><a href="' . admin_url() . '">Admin</a></li>';
-        
-    
-        
-        // Insérez "Admin" entre "Nous rencontrer" et "Commander"
-        // $items .= $admin_li;
+
+        $items .= '<li class="menu-item menu-item-type-post_type menu-item-object-page"><a href="' . admin_url() . '">Admin</a></li>';
     }
     return $items;
 }
 
-add_filter( 'wp_nav_menu_items', 'ajouter_lien_admin_menu_astra', 10, 2 );
+add_filter( 'wp_nav_menu_items', 'ajouter_lien_admin_menu_astra', 20, 2 );
+
+add_filter( 'astra_get_mobile_menu_items', 'ajouter_lien_admin_menu_astra', 20, 2 );
+
+
 
 
 
